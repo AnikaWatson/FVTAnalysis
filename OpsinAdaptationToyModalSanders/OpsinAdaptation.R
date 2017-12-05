d19b8bd002ece69a7bd7563b077933f15550de44
AnikaWatson <abwatson@questu.ca>
2017-12-05 11:45:28 -0800

    Opsin Sensitivity Curves
    Translated code from Mathematica to define and plot the two opsin
    sensitivity curves of the toy fish.

--- OpsinAdaptationToyModalSanders/OpsinAdaptation.R
+++ OpsinAdaptationToyModalSanders/OpsinAdaptation.R
@@ -1,10 +1,10 @@
-#---- Preliminaries----
+#---- Preliminaries ----
 #opsin labels and Subscript[\[Lambda], max] values for their sensitivity curves*)
 
 wd <- getwd()
 
 #check if output folder already exists otherwise create one
-folders <- "OutputData"
+folders <- "data_output"
 if (file.exists(folders) == FALSE) {
   dir.create(file.path(wd, folders), showWarnings = FALSE) 
 } else print("Output Folder Already exists")
@@ -28,34 +28,39 @@ nFunc <- length(functions)
 la <- 200
 lb <- 800
 
-Aband1 <- function(l,L) {
+#---- Definition of Opsin Sensitivity Curves ----
+
+Aband1 <- function(x,L) {
   a <- (0.8795 + 0.0459*exp(-(L - 300.)^(2/11940.)))
-  1.0/(exp(69.7 (a - (L/l))) + exp(28. (0.922 - (L/l))) + 
-         exp(-14.9 (1.104 - (L/l))) + 0.674)
+  1.0/(exp(69.7*(a - (L/x))) + exp(28.*(0.922 - (L/x))) + 
+         exp(-14.9*(1.104 - (L/x))) + 0.674)
 }
 
-Bband1 <- function(l,L) {
+Bband1 <- function(x,L) {
   m <- 189. + 0.315*L
   b <- -40.5 + 0.195*L
-  0.26*exp(-((l - m)/b)^2)
+  0.26*exp(-((x - m)/b)^2)
 }
 
-Aband2 <- function(l,L) {
-  x <- lmax/l
+Aband2 <- function(x,L) {
+  l <- lmax/x
   a <- 0.8795 + 0.0268*exp((L - 665.)/40.7)
   A <- 62.7 + 1.834*exp((L - 625.)/54.2)
-  1.0/(Exp[A (a - x)] + Exp[20.85 (0.9101 - x)] + 
-         Exp[-10.37 (1.1123 - x)] + 0.5343)
+  1.0/(exp[A*(a - l)] + exp[20.85*(0.9101 - l)] + 
+         exp[-10.37*(1.1123 - l)] + 0.5343)
 }
 
-Bband2 <- function(l,L) {
+Bband2 <- function(x,L) {
   m <- 216.7 + 0.287*L
   b <- 317. - 1.149*L + 0.00124*L^2
-  0.37*exp(-((l - m)/b)^2)
+  0.37*exp(-((x - m)/b)^2)
 }
 
-opsinSpectra <- table(Aband1(l, lmax((i))) + Bband1(l, lmax((i))),  {i, 1, nOpsin})
 
-for(i in 1:nOpsin) {
-  
-}
+pdf(paste(outputPath, "OpsinSpectra.pdf", sep = ""))
+curve(Aband1(x, lmax[1]) + Bband1(x, lmax[1]), la, lb, 101, ylab = "", xlab = "", col = "red")
+par(new=TRUE)
+curve(Aband1(x, lmax[2]) + Bband1(x, lmax[2]), la, lb, 101, ylab = "", xlab = "", col = "blue", axes = FALSE)
+legend(199, 1, legend = opsins, col=c("red", "blue"), lty=1, cex=0.8)
+dev.off()
+
